d5676a2089049d52f58180ac928471c345fb8238
paul geana <87538456+1234pau@users.noreply.github.com>
2022-06-27 13:26:52 +0300

    ,

---
+++ script.js
@@ -0,0 +1,68 @@
+const input = document.querySelector('.task')
+const button = document.querySelector('.add')
+const container = document.querySelector('.container')
+const para2 = document.querySelector('.para2')
+
+button.addEventListener('click', addItem)
+
+let para
+let iconDelete
+let iconDone
+let div
+
+function addItem() {
+
+    //---------div container---------
+    div = document.createElement('div')
+    div.classList.add("divContainer")
+    container.appendChild(div)
+        //---------paragraf---------
+    para = document.createElement('p')
+    para.classList.add('para')
+    div.appendChild(para)
+        //---------icon check---------
+    iconDone = document.createElement('button')
+    iconDone.classList.add("done")
+    div.appendChild(iconDone)
+    iconDone.innerHTML = `<i class="fa-solid fa-check"></i>`
+        //---------icon delete---------
+    iconDelete = document.createElement('button')
+    iconDelete.classList.add("delete")
+    div.appendChild(iconDelete)
+    iconDelete.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+        //---------stuf---------
+
+    const value = input.value
+    const value2 = value.charAt(0).toUpperCase() + value.slice(1);
+    para.textContent = `${value2}.`
+    input.value = ''
+    input.focus()
+    remove(iconDelete, iconDone, para, div)
+    lineThrough(iconDone, para, div)
+
+}
+//   if(input.value === ''){
+//   button.removeEventListener('click', addItem)
+//     para2.innerHTML = 'ddd'
+//   }else{
+//    addItem()
+// } 
+
+function remove(buttonDel, button, para, div) {
+    buttonDel.addEventListener('click', function() {
+        this.remove()
+        button.remove()
+        para.remove()
+        div.remove()
+    })
+}
+
+function lineThrough(item, para, div) {
+    item.addEventListener('click', function() {
+        para.classList.add('line')
+        div.style.opacity = 0.6
+    })
+}
+
+
+//input.focus()
